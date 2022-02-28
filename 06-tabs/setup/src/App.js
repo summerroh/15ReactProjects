ee9980564f3f317a88e48f8fef26209fae215596
summerroh <summerrohh@gmail.com>
2022-02-28 22:00:50 +1100

    06-tabs done

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,69 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobDatas, setJobDatas] = useState([]);
+  const [value, setValue] = useState(0);
+  
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobDatas(newJobs)
+    setLoading(false)
+  }
+  
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  
+  //when fetching API, I need to set up  a loading function to deal with before the data is loaded.
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading . . . </h1>
+      </section>
+    )
+  }
+
+  const { title, company, dates, duties } = jobDatas[value]
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {jobDatas.map((jobData, index) => {
+            return (
+              <button key={jobData.id}
+                      onClick={() => setValue(index)}
+                      className={`job-btn ${ index === value && 'active-btn'}`}
+                      >
+                {jobData.company}
+              </button>
+            )
+          })}
+       </div>
+
+      <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className='job-date'>{dates}</p>
+            {duties.map( (duty, index) => {
+              return(
+              <div key={index} className='job-desc'> 
+                <FaAngleDoubleRight className='job-icon'/>
+                <p>{duty}</p>
+              </div> )
+            })}
+      </article>
+      </div>
+
+    </section>
+    )
 }
 
 export default App
